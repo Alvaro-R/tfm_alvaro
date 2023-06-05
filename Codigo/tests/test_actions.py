3621dc46e559088da9d5929c4f2d55136aa03afa
Alvaro-R <loman244@gmail.com>
2023-06-05 21:18:48 +0200

    Dummy pytest

---
+++ Codigo/tests/test_actions.py
@@ -0,0 +1,10 @@
+def test_dummy():
+    """
+    Test the addition of two numbers.
+
+    This is a dummy test to check pytest pipeline works properly.
+
+    Returns:
+        None
+    """
+    assert 1 + 1 == 2
